40513296d5aa965362022366ba36d2da0ce8e470
randy sartor <randal.sartor@gmail.com>
2018-12-12 16:00:20 -0500

    added Box states

--- src/blue-box.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './blue-box.css'
-
-export default function BlueBox() {
-  return <div className='blue-box'></div>
-}
--- src/green-box.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './green-box.css'
-
-export default function GreenBox() {
-  return <div className='green-box'></div>
-}
