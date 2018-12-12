4c27c399d50ae6648ec5fea7302a958d46f9be4b
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-12 11:16:25 -0500

    style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 import Button from './Button';
 import ColorBox from './ColorBox';
 
-export default function App() {
+// export default class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       backgroundColor: rgb(0, 0, 225)
+//     }
+//   }
+
+//   return (
+//   <div className="app">
+//     <ColorBox />
+//     <Button />
+//   </div>)
+
+// }  
+
+export default function App()  {
 
   return (
   <div className="app">
